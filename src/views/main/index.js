e534898fc6880ad4747e8581749d535bdf477fca
Lâm Xuân Nguyên <100450698+imnguin@users.noreply.github.com>
2024-10-08 14:49:41 +0700

    commit

--- src/views/main/index.js
+++ src/views/main/index.js
@@ -8,93 +8,55 @@ import Home from '../home'
 import Chats from '../chats'
 
 const Main = (props) => {
-  const renderItem = (name) => {
-    switch (name) {
-      case 'Account':
-        return <Account navigation={props.navigation} />
-      case 'Order':
-        return <Order navigation={props.navigation} />
-      case 'Home':
-        return <Home navigation={props.navigation} />
-      case 'Chats':
-        return <Chats navigation={props.navigation} />
-      default:
-        return <View style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-          <Text>{name}</Text>
-        </View>
-    }
+  const element = {
+    account: <Account navigation={props.navigation} />,
+    order: <Order navigation={props.navigation} />,
+    home: <Home navigation={props.navigation} />,
+    chats: <Chats navigation={props.navigation} />
   }
+
+  const renderItem = (name) => {
+    return element[name.toLowerCase()] || <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}><Text>{name}</Text></View>;
+  }
+
   return (
     <>
       <StatusBar style="auto" />
       {
-        props.name != 'Account' && <View
-          style={{
-            height: 95,
-            backgroundColor: '#F46138',
-            flexDirection: 'row',
-            alignItems: 'flex-end',
-            paddingLeft: 15,
-            paddingRight: 15,
-            paddingBottom: 15
-          }}>
-          <View style={{
-            flexDirection: 'row',
-            gap: 15,
-          }}>
+        props.name.toLowerCase() != 'account' &&
+        <View
+          style={{ height: 95, backgroundColor: '#F46138', flexDirection: 'row', alignItems: 'flex-end', paddingLeft: 15, paddingRight: 15, paddingBottom: 15 }}>
+          <View style={{ flexDirection: 'row', gap: 15, }}>
             <TouchableOpacity>
               <Image
                 source={require('../../../assets/211818_search_icon.png')}
                 resizeMode='contain'
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: '#fff',
-                }}
+                style={{ width: 25, height: 25, tintColor: '#fff', }}
               />
             </TouchableOpacity>
             <TextInput
               placeholder="Tìm kiếm"
               placeholderTextColor={'#fff'}
-              style={{
-                borderRadius: 7,
-                flex: 1,
-                fontSize: 15,
-                color: '#fff'
-              }}
+              style={{ borderRadius: 7, flex: 1, fontSize: 15, color: '#fff' }}
             />
             <TouchableOpacity>
               <Image
                 source={require('../../../assets/211693_bell_icon.png')}
                 resizeMode='contain'
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: '#fff'
-                }}
+                style={{ width: 25, height: 25, tintColor: '#fff' }}
               />
             </TouchableOpacity>
             <TouchableOpacity>
               <Image
                 source={require('../../../assets/8545624_qr_code_scan_program_barcode_icon.png')}
                 resizeMode='contain'
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: '#fff'
-                }}
+                style={{ width: 25, height: 25, tintColor: '#fff' }}
               />
             </TouchableOpacity>
           </View>
         </View>
       }
-      <View style={styles.container}>
-        {renderItem(props.name)}
-      </View>
+      <View style={styles.container}>{renderItem(props.name)}</View>
     </>
   )
 }
